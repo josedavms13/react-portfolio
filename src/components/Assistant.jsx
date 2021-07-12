7f5c1f458525a500941b2f5d51556b95a262a8ed
josedavms13 <josedav0ms13@gmail.com>
2021-07-12 12:31:24 -0500

    FEAT:
    when click on blue rectangle tracks the mouse position
    
    next move with mouse movement

--- src/components/Assistant.jsx
+++ src/components/Assistant.jsx
@@ -1,24 +1,46 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 import './componentsCSS/Assistant.css'
 
 
+const Assistant = ({props}) => {
+
+    const [clicked, SetClicked] = useState(false)
+    const [mousePositionWhileClick, SetMousePositionWhileClick] = useState([0, 0])
+    const [position, setPosition] = useState([0, 0])
 
 
+    let mousePosition = [0, 0];
 
-const Assistant =({props})=>{
+    function mousePositionWhenClickFunction(e) {
+        if (clicked) {
+            mousePosition[0] = e.clientX;
+            mousePosition[1] = e.clientY;
+            SetMousePositionWhileClick(mousePosition)
+        }
 
-    const [position , setPosition] = useState([0, 0, 0, 0])
+    }
 
 
+    useEffect(() => {
+
+        console.log((mousePositionWhileClick))
+        setPosition(mousePositionWhileClick)
+
+    }, [mousePositionWhileClick])
+
+    useEffect(() => {
+        console.log(clicked);
+    }, [clicked])
 
 
     return (
-        <div className={'assistant'} >
-            <div className="assistant-container">
+        <div className={'assistant'}>
+            <div className="assistant-container" onMouseLeave={() => SetClicked(false)}
+                 onMouseDownCapture={() => SetClicked(true)} onMouseUp={() => SetClicked(false)}
+                 onMouseMoveCapture={(event => mousePositionWhenClickFunction(event))}>
 
             </div>
-            <div className="circle"></div>
 
 
         </div>
